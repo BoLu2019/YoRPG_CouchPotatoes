3fbbcd0171abf1fb8e65e2db2b509bbd6dfcb538
BoLu2019 <blu4@stuy.edu>
2017-11-13 22:55:38 -0500

    Add Wizard

---
+++ v3/Wizard.java
@@ -0,0 +1,27 @@
+/*
+  Bo Hui Lu, Larry Wong, Richard Wong (Team CouchPotatoes)
+  APCS1 pd08
+  HW31 --Ye Olde Role Playing Game, Expanded .
+  2017-11-14
+*/
+
+public class Wizard extends Protagonist{
+
+    public Wizard(){
+	health = 125;
+	strength = 100;
+	defense = 40;
+	attRating = 0.8;
+
+
+    }//ends constructor
+
+    //about this class
+    public static String about(){
+	return "This subclass Wizard is a Protagonist with exceptionally high magical abilities, hence increasing attRating by 2x. Does HUGEEEE damage, so watch out.";
+
+    }//end about()
+
+    
+
+}//ends class
