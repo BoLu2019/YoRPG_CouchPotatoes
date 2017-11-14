ac861ce45f477a68cafc08aa8d2afcec2af3d794
BoLu2019 <blu4@stuy.edu>
2017-11-14 14:01:50 -0500

    Add abstract

--- v4/Protagonist.java
+++ v4/Protagonist.java
@@ -1,11 +1,11 @@
-/*
+ /*
   Bo Hui Lu, Larry Wong, Richard Wong (Team CouchPotatoes)
   APCS1 pd08
   HW28 -- Ye Olde Role Playing Games
   2017-11-09
 */
 
-public class Protagonist extends Character{
+public abstract class Protagonist extends Character{
 
     protected String name = "YouForgotYourself";
 
@@ -37,18 +37,15 @@ public class Protagonist extends Character{
     }
 
     //prepares for special attack. Lowered defense for a 100% boost in attack
-    public void specialize(){
-	defense -= 10;
-	attRating *= 1.2;     
-    }
+    public abstract void specialize();
+    
+    
 
     //resets attRating and defense to normal
-    public void normalize(){
-	defense = 40;
-	attRating = 0.4	;
-    }
+    public abstract void normalize();
 
     
 
-}//ends class
+    public abstract String about();
 
+    
